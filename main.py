32447d66848ee30cedb4eba741d0a95f98dfa77b
wilhio <benjamin.ben.adams@gmail.com>
2025-05-11 15:18:17 -0400

    inital commits

---
+++ main.py
@@ -0,0 +1,13 @@
+from helper import calculate_weight, calculate_difficulty
+
+exercise = input("Enter the exercise: ")
+
+input_difficulty = input("How difficult is this exercise? ")
+
+result = calculate_difficulty(input_difficulty, exercise)
+print(result)
+
+
+
+
+
