6757ccb16e39a5b25599a0221071bd27295edfa3
wilhio <benjamin.ben.adams@gmail.com>
2025-05-11 22:46:35 -0400

    Delete helper.py

--- helper.py
+++
@@ -1,7 +0,0 @@
-
-def calculate_difficulty(difficulty, exercise_name):
-    if difficulty == "easy":
-        return f"next week add 10lbs to {exercise_name} exercise"
-    elif difficulty == "medium":
-        return f"next week add 5lbs to {exercise_name} exercise"
-    
